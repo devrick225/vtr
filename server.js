087315c7825e29f48b38a6cc23e7d51117f38ef6
Koffi Eric Rainier <erickoffi29@gmail.com>
2023-10-10 20:38:16 +0000

    Update server
    test

--- server.js
+++ server.js
@@ -1,18 +1,12 @@
 require('dotenv').config()
-const https = require('https');
+const http = require('http');
 const app  = require('./app/app');
 require('./config/dbConnect');
-const fs = require('fs');
-
-const options = {
-    key: fs.readFileSync('./cert/pac.bj_sansmot.key'),
-    cert: fs.readFileSync('./cert/pac.bj.crt'),
-};
 const PORT = process.env.PORT || 3005;
 
 //middleware
 
 
 //server
-const server = https.createServer(options,app);
+const server = http.createServer(app);
 server.listen(PORT, console.log(`Server is running on port ${PORT}`))
