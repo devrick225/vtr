05b529bbbdd36a317ba24295c6f7fe7e5f3aaf5e
Koffi Eric Rainier <erickoffi29@gmail.com>
2025-01-30 01:00:31 +0000

    update server to https

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const cred = {
 //middleware
 
 //server
-const server = http.createServer(app);
-//const httpsServer = https.createServer(cred, app)
-server.listen(PORT, console.log(`Server http is running on port ${PORT}`))
-//httpsServer.listen(443, console.log(`Server https is running on port ${PORT}`))
+//const server = http.createServer(app);
+const httpsServer = https.createServer(cred, app)
+//server.listen(PORT, console.log(`Server http is running on port ${PORT}`))
+httpsServer.listen(443, console.log(`Server https is running on port ${PORT}`))
